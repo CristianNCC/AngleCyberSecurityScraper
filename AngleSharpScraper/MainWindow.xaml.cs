392869899fc7958e972541c45fb7c1829080f25c
CristianNCC <nicolaecristiancatalin@gmail.com>
2019-09-29 22:41:56 +0300

    Refactored the code for HackerNews and all around improvements to the GUI.

--- AngleSharpScraper/MainWindow.xaml.cs
+++ AngleSharpScraper/MainWindow.xaml.cs
@@ -9,33 +9,100 @@ using AngleSharp.Html.Dom;
 using System.IO;
 using AngleSharp.Dom;
 using AngleSharp.Text;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Diagnostics;
+using System.Windows.Navigation;
 
 namespace AngleSharpScraper
 {
     public partial class MainWindow : Window
     {
-        private string siteUrl = "https://www.oceannetworks.ca/news/stories";
+        private string siteUrl = "https://thehackernews.com/";
+        private int numberOfPages = 50;
 
         private string ArticleTitle { get; set; }
         private string Url { get; set; }
-        public string[] QueryTerms { get; } = { "Ocean", "Nature", "Pollution" };
+        public string[] QueryTerms { get; } = { "SIM", "iOS", "Exploit", "APT Map" };
+
+        public Dictionary<string, List<Tuple<string, string>>> termToScrapeDictionary = new Dictionary<string, List<Tuple<string, string>>>();
 
         internal async void ScrapeWebsite()
         {
             CancellationTokenSource cancellationToken = new CancellationTokenSource();
             HttpClient httpClient = new HttpClient();
-            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
-            cancellationToken.Token.ThrowIfCancellationRequested();
-
-            resultsTextBox.Text = "";
-            spinnerControl.Visibility = System.Windows.Visibility.Collapsed;
-
-            Stream response = await request.Content.ReadAsStreamAsync();
-            cancellationToken.Token.ThrowIfCancellationRequested();
-
             HtmlParser parser = new HtmlParser();
-            IHtmlDocument document = parser.ParseDocument(response);
-            GetScrapeResults(document);
+
+            resultsStackPanel.Children.Clear();
+
+            for (int iPageIdx = 0; iPageIdx < numberOfPages; iPageIdx++)
+            {
+                HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+                cancellationToken.Token.ThrowIfCancellationRequested();
+
+                Stream response = await request.Content.ReadAsStreamAsync();
+                cancellationToken.Token.ThrowIfCancellationRequested();
+
+                IHtmlDocument document = parser.ParseDocument(response);
+                GetScrapeResults(document);
+
+                siteUrl = document.All.Where(x => x.ClassName == "blog-pager-older-link-mobile")
+                    .FirstOrDefault()?
+                    .OuterHtml.ReplaceFirst("<a class=\"blog-pager-older-link-mobile\" href=\"", "")
+                    .ReplaceFirst("\" id", "*")
+                    .Split('*').FirstOrDefault();
+
+                if (string.IsNullOrEmpty(siteUrl))
+                    break;
+            }
+
+            foreach (var termToScrape in termToScrapeDictionary)
+            {
+                List<Tuple<string, string>> termResults = termToScrape.Value;
+
+                GroupBox termGroupBox = new GroupBox()
+                {
+                    Header = termToScrape.Key,
+                    Content = new StackPanel()
+                    {
+                        Orientation = Orientation.Vertical,
+                        Margin = new Thickness(5, 5, 5, 5)
+                    }
+                };
+
+                resultsStackPanel.Children.Add(termGroupBox);
+
+                for (int iTermResult = 0; iTermResult < termResults.Count; iTermResult++)
+                {
+                    TextBlock title = new TextBlock()
+                    {
+                        Text = termToScrapeDictionary[termToScrape.Key][iTermResult].Item1
+                    };
+
+                    (termGroupBox.Content as StackPanel).Children.Add(title);
+
+                    Hyperlink hyperlink = new Hyperlink();
+                    hyperlink.Inlines.Add(termToScrapeDictionary[termToScrape.Key][iTermResult].Item2);
+                    hyperlink.NavigateUri = new Uri(termToScrapeDictionary[termToScrape.Key][iTermResult].Item2);
+                    hyperlink.RequestNavigate += Hyperlink_RequestNavigate;
+
+                    TextBlock urlTextBlock = new TextBlock();
+                    urlTextBlock.Inlines.Add(hyperlink);
+                    urlTextBlock.Margin = new Thickness(5, 5, 5, 10);
+
+                    (termGroupBox.Content as StackPanel).Children.Add(urlTextBlock);
+                }
+            }
+
+            spinnerControl.Visibility = System.Windows.Visibility.Collapsed;
+            httpClient.Dispose();
+            cancellationToken.Dispose();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
 
         private void GetScrapeResults(IHtmlDocument document)
@@ -43,38 +110,38 @@ namespace AngleSharpScraper
             List<IElement> articleLink = new List<IElement>();
             foreach (var term in QueryTerms)
             {
-                articleLink = document.All.Where(x => x.ClassName == "views-field views-field-nothing" && (x.ParentElement.InnerHtml.Contains(term) || x.ParentElement.InnerHtml.Contains(term.ToLower()))).ToList();
-                resultsTextBox.Text += "----- " + term + " -----" + Environment.NewLine;
-                PrintResults(term, articleLink);
+                articleLink = document.All.Where(x => x.ClassName == "story-link" && (x.InnerHtml.Contains(term) || 
+                        x.InnerHtml.Contains(term.ToLower()))).ToList();
+
+                FillResultsDictionary(term, articleLink);
             }
         }
 
-        public void PrintResults(string term, IEnumerable<IElement> articleLink)
+        public void FillResultsDictionary(string term, IEnumerable<IElement> articleLink)
         {
             foreach (var result in articleLink)
             {
                 CleanUpResults(result);
                 if (!string.IsNullOrWhiteSpace(ArticleTitle) && !string.IsNullOrWhiteSpace(Url))
-                    resultsTextBox.Text += $"{ArticleTitle}{Environment.NewLine} - {Url}{Environment.NewLine}{Environment.NewLine}";
+                {
+                    string currentLine = $"{ArticleTitle}{Environment.NewLine} " +
+                        $"- {Url}{Environment.NewLine}{Environment.NewLine}";
+
+                    if (!termToScrapeDictionary.ContainsKey(term))
+                        termToScrapeDictionary[term] = new List<Tuple<string, string>>();
+
+                    termToScrapeDictionary[term].Add(new Tuple<string, string>(ArticleTitle, Url));
+                }
             }
         }
 
         private void CleanUpResults(IElement result)
         {
-            if (result.InnerHtml.Contains("<span class=\"field-content\"><div><a href=\""))
-            {
-                string htmlResult = result.InnerHtml.ReplaceFirst("<span class=\"field-content\"><div><a href=\"", "https://www.oceannetworks.ca");
-                htmlResult = htmlResult.ReplaceFirst("\">", "*");
-                htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
-                htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span>  ", "");
-
-                SplitResults(htmlResult);
-            }
-            else
-            {
-                ArticleTitle = string.Empty;
-                Url = string.Empty;
-            }
+            string htmlResult = result.OuterHtml.ReplaceFirst("<a class=\"story-link\" href=\"", "");
+            htmlResult = htmlResult.ReplaceFirst("\">", "");
+            htmlResult = htmlResult.ReplaceFirst("\n<div class=\"clear home-post-box cf\">\n<div class=\"home-img clear\">\n<div class=\"img-ratio\"><img alt=\"", " * ");
+            htmlResult = htmlResult.ReplaceFirst("\" class=\"home-img-src lazyload\"", "*");       
+            SplitResults(htmlResult);
         }
 
         private void SplitResults(string htmlResult)
